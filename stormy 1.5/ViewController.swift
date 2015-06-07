acc9c1314fa9763f8272ddf4dbba314f3ead4cb9
Michael Elbaz <Michael@Michaels-MacBook-Pro.local>
2015-06-07 15:06:52 -0400

    Initial Commit

---
+++ stormy 1.5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stormy 1.5
+//
+//  Created by Michael Elbaz on 6/7/15.
+//  Copyright (c) 2015 elboss-apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ stormy 1.5Tests/stormy_1_5Tests.swift
@@ -0,0 +1,36 @@
+//
+//  stormy_1_5Tests.swift
+//  stormy 1.5Tests
+//
+//  Created by Michael Elbaz on 6/7/15.
+//  Copyright (c) 2015 elboss-apps. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class stormy_1_5Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
